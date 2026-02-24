b40bd2a136b300853fa1feef84de4f1e3f98bf2e
Oluwatodunni <obafemitodunni@gmail.com>
2026-02-24 09:06:19 +0100

    updates

--- normalization/name_extractor.py
+++ normalization/name_extractor.py
@@ -1,14 +1,15 @@
 import os
-import requests
 from openai import OpenAI
+import streamlit as st
+from dotenv import load_dotenv
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
-
-def is_cloud():
-    """Detect if running on hosted environment"""
-    return os.getenv("OPENAI_API_KEY") is not None
-
-
+def get_openai_key():
+    try:
+        return st.secrets["OPENAI_API_KEY"]
+    except:
+        load_dotenv()
+        return os.getenv("OPENAI_API_KEY")
+    
 def extract_name(text):
 
     if not text or len(text) < 5:
@@ -17,55 +18,25 @@ def extract_name(text):
     # --------------------------
     # CLOUD MODE → OpenAI
     # --------------------------
-    if is_cloud():
+    client = OpenAI(api_key=get_openai_key())
 
-        client = OpenAI()
+    prompt = f"""
+        Extract ONLY the customer's full name from this bank transaction text.
 
-        prompt = f"""
-            Extract ONLY the customer's full name from this bank transaction text.
+        Rules:
+        - Return ONLY the name
+        - No explanations
+        - No extra words
 
-            Rules:
-            - Return ONLY the name
-            - No explanations
-            - No extra words
+        Text:
+        {text}
+        """
 
-            Text:
-            {text}
-            """
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",   # fast + cheap
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0
+    )
 
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",   # fast + cheap
-            messages=[{"role": "user", "content": prompt}],
-            temperature=0
-        )
+    return response.choices[0].message.content.strip()
 
-        return response.choices[0].message.content.strip()
-
-
-    # --------------------------
-    # LOCAL MODE → Ollama
-    # --------------------------
-    else:
-
-        prompt = f"""
-            Extract ONLY the customer's full name from this bank transaction text.
-
-            Rules:
-            - Return ONLY the name
-            - No explanations
-            - No extra words
-
-            Text:
-            {text}
-            """
-
-        response = requests.post(
-            OLLAMA_URL,
-            json={
-                "model": "llama3",
-                "prompt": prompt,
-                "stream": False
-            }
-        )
-
-        return response.json()["response"].strip()
